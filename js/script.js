92d53a498ad623cafd115c58825efe6944a12d91
wenglesUx <85462594+wenglesUx@users.noreply.github.com>
2023-03-02 20:26:19 -0300

    Reestruturação
    Adição de interações e novos detalhes na interface

--- js/script.js
+++ js/script.js
@@ -1,30 +1,39 @@
-
+//variaveis
 let button = document.querySelector('#button-search');
 let display = document.querySelector('#display-area');
 let desc = document.querySelector('#description');
-let desarea = document.querySelector('#description-area');
+let descArea = document.querySelector('#description-area');
 
-let menus = document.querySelector('#hidde-menu')
-let infos = document.querySelector('#area')
-let sobre = document.querySelector('#info')
+const menus = document.querySelector('#hidde-menu')
+const inforArea = document.querySelector('#area')
+const sobre = document.querySelector('#info')
+const close = document.querySelector('#close')
 
 
+//Eventos
 
 button.addEventListener('click',submit)
 
+menus.addEventListener('click',()=>{
+   inforArea.classList.toggle('opacit')
+   
+});
+
+close.addEventListener('click',()=>{
+    inforArea.classList.toggle('opacit')
+});
+//Funções
 function submit(e){
     e.preventDefault()
-    newctt = '';
+ 
     textinfo = document.querySelector('#text-area').value
 
   
 let generate = `<img src ="https://source.unsplash.com/600x500/?${textinfo}">`
 display.innerHTML = generate
-desc.innerText = textinfo
-desarea.style.display = 'block'
+
+desc.innerText = `Imagem relacionada a sua pesquisa sobre ${textinfo}`
+
+descArea.style.display = 'block'
 }
 
-menus.addEventListener('click',()=>{
-    
-    infos.classList.toggle("opacity")
-})
